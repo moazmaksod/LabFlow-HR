d95c373f4812fd0a95e2798bcbe298e08ad89b46
MoazMaksod <moaz.maksod@gmail.com>
2026-06-07 20:38:39 +0300

    Remove unused location library and test attendance log file

--- test-attendance-log.ts
+++
@@ -1,8 +0,0 @@
-import db from './server/db/index.js';
-const reqs = db.prepare("SELECT * FROM requests WHERE type = 'overtime_approval'").all() as any[];
-console.log("Requests:");
-console.log(reqs);
-
-const atts = db.prepare("SELECT * FROM attendance").all() as any[];
-console.log("Attendance:");
-console.log(atts);
